da2235f8ab7ee1ef6e851793c0c2d99713c3e5f5
Filip Balucha <balucha.filip2@gmail.com>
2020-01-27 19:52:45 +0000

    Implement first working version of slanted button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { faRuler } from "@fortawesome/free-solid-svg-icons";
+import Button from "./components/Button.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button
+        caption="CALIBRATE"
+        icon={faRuler}
+        onClick={() => {
+          return;
+        }}
+        width="25%"
+        slantedLeft
+        // slantedRight
+        textColor="#FFFFFF"
+        backgroundColor="#1098AD"
+      ></Button>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Button.css";
+
+export default function Button(props) {
+  // known issue: both sides cannot be slanted
+
+  const buttonSkewClassName = `button-slant ${
+    props.slantedLeft ? "slant__left slant-margin__left" : ""
+  } ${props.slantedRight ? "slant__right slant-margin__right" : ""}`;
+
+  const buttonUnskewClassName = `${props.slantedLeft ? "slant__right" : ""} ${
+    props.slantedRight ? "slant__left" : ""
+  }`;
+
+  return (
+    <div
+      className="button-container"
+      onClick={props.onClick}
+      style={{ width: props.width }}
+    >
+      <div
+        className={buttonSkewClassName}
+        style={{
+          color: props.textColor,
+          backgroundColor: props.backgroundColor
+        }}
+      >
+        <div className={buttonUnskewClassName}>
+          <FontAwesomeIcon className="button-icon" icon={props.icon} />
+          <span>{props.caption}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
