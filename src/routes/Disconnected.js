6e87b2c87d1acbe9ecfb7fdfa83098cadf960dd7
Robertas Norkus <robertas.nork@gmail.com>
2020-02-19 00:39:56 +0000

    Implement Disconnected

--- src/routes/Disconnected.js
+++ src/routes/Disconnected.js
@@ -1,3 +1,23 @@
+import React from "react";
+import "./Disconnected.css";
+import SetupLogo from "./SetupLogo";
+import { faExclamationTriangle, faSync } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "../components/Button";
+
 export default function Disconnected() {
-  return("disconnected");
+  const handleClick = () => {
+    console.log("reconnect");
+  }
+
+  return (
+    <div className="disconnected-wrapper centered">
+      <SetupLogo></SetupLogo>
+      <div className="disconnected-wrapper-content">
+        <FontAwesomeIcon className="icon" icon={faExclamationTriangle}></FontAwesomeIcon>
+        There is no connection to the base-station.
+      </div>
+      <Button caption="RETRY" handleClick={handleClick} backgroundColor="bg-white-gradient" icon={faSync}></Button>
+    </div>
+  );
 }
