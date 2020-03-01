61104e1f4ec2370acc6691bdbe522f808f91902a
Filip Balucha <balucha.filip2@gmail.com>
2020-03-01 22:54:16 +0000

    Add support for multiple charts and implement functions to add and remove charts

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tabs.css";
 import LineChart from "./LineChart";
 
+const MAX_CHARTS = 4;
+
 export default function Tabs(props) {
+  const [selectingDatapoints, setSelectingDataPoints] = useState(false);
+  var config = require("./config.json");
+
+  const removeChart = id => {
+    config.charts = config.charts.filter(chart => chart.id !== id);
+  };
+
+  const getLineCharts = () => {
+    return Array.from(config.charts, chart => (
+      <LineChart
+        key={chart.id}
+        id={chart.id}
+        datapoints={props.graphs[0].datasets}
+        fontSize={props.graphs[0].fontSize}
+        removeChart={removeChart}
+        selectDatapoints={selectDatapoints}
+      />
+    ));
+  };
+
+  const addLineChart = () => {
+    if (config.charts.length >= MAX_CHARTS) {
+      console.log(`Maximum number of charts (${MAX_CHARTS}) reached!`);
+      return;
+    }
+    var id = config.charts.length;
+    config.charts.push({ id, datapoints: [] });
+  };
+
+  const selectDatapoints = chartId => {
+    setSelectingDataPoints(!selectingDatapoints);
+    // selectingDatapoints = true;
+    console.log("selecting");
+  };
+
   return (
     <div className="tabs-root">
       <div className="tabs-container"></div>
+
       <div className="window-container">
-        {/* <LineChart
-          datasets={[
-            { label: "IMU", data: IMUData, unit: "m/s²" },
-            { label: "Speed", data: speedData, unit: "m/s" }
-          ]}
-          fontSize={12}
-        /> */}
-        <LineChart
-          datasets={props.graphs[0].datasets}
-          fontSize={props.graphs[0].fontSize}
-        ></LineChart>
+        <div className="graph-container">{getLineCharts()}</div>
+        <div className="plus" onClick={() => addLineChart()}></div>
       </div>
+      {/* <div className={selectingDatapoints ? "datapoint-container" : ""}></div> */}
     </div>
   );
 }
