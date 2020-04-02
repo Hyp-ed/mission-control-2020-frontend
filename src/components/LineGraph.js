e9f5f8a43f31b58d495f5c716127cbf0c2db8fcc
Filip Balucha <balucha.filip2@gmail.com>
2020-04-02 15:51:13 +0200

    Remove local appearance modification; make legend labels more specific

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -6,8 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { isEqual } from "lodash";
 import { getDataPointValue } from "../DataTools";
+import { capitalize } from "lodash";
 
-const graph_colors = [
+const COLORS = [
   "#BF000B", // dark red
   "#009AB0", // blue
   "#FF9B00", // yellow
@@ -22,66 +23,6 @@ const graph_colors = [
   "#FEFF99", // lemon
   "#4BBEBE" // green
 ];
-const accentColor = "#8F8F8F";
-
-/**
- * @param {number} fontSize
- * @returns an object that changes the look of ChartJS graphs
- */
-const options = fontSize => {
-  return {
-    responsive: true,
-    maintainAspectRatio: true,
-    aspectRatio: 2,
-    animation: {
-      duration: 0 // ensure that the lines scale with y-axis on y-axis resize without delay
-    },
-    elements: {
-      point: {
-        radius: 0
-      }
-    },
-    style: {
-      strokeWidth: 1 // line width
-    },
-    legend: {
-      labels: {
-        fontColor: accentColor,
-        fontFamily: "Roboto Mono",
-        fontSize: fontSize
-      }
-    },
-    scales: {
-      xAxes: [
-        {
-          realtime: {
-            ttl: undefined // automatically delete data after it disappears off the graph
-          },
-          ticks: { display: false },
-          gridLines: { display: false },
-          type: "time",
-          distribution: "linear",
-          time: { parser: "DD/MM/YYYY" }
-        }
-      ],
-      yAxes: [
-        {
-          ticks: {
-            display: true,
-            fontSize: fontSize,
-            fontFamily: "Roboto Mono",
-            fontColor: accentColor
-          },
-          gridLines: {
-            display: true,
-            color: accentColor,
-            zeroLineColor: accentColor // color x-axis
-          }
-        }
-      ]
-    }
-  };
-};
 
 /**
  * React effect hook that deep compares objects in dependency array
@@ -104,54 +45,41 @@ const useDeepEffect = (fn, deps) => {
   }, deps);
 };
 
+const IGNORE_KEY = "crucial_data";
+const DELIMITER = " > ";
+
 export default function LineGraph(props) {
-  const [datasets, setDatasets] = useState({});
+  const [pathData, setPathData] = useState({});
 
   useDeepEffect(() => {
-    setDatasets(old_dataset => {
+    setPathData(oldData => {
       props.paths.forEach(path => {
-        let datapoint = {
+        let dataPoint = {
           x: props.data.time,
           y: getDataPointValue(props.data.telemetryData, path)
         };
-        if (old_dataset.hasOwnProperty(path)) {
-          old_dataset[path] = [...old_dataset[path], datapoint];
+        if (oldData.hasOwnProperty(path)) {
+          oldData[path] = [...oldData[path], dataPoint];
         } else {
-          old_dataset[path] = [datapoint];
+          oldData[path] = [dataPoint];
         }
       });
-      return old_dataset;
+      return oldData;
     });
-  }, [props.data, props.paths]);
+  }, [props.data]);
 
-  /**
-   * Generates an unambiguous label for a datapoint
-   * If no two datapoints share the same name, the datapoint name is returned
-   * If multiple datapoints share the same name, the previous key from path array is prepended to the datapoint names
-   * Example: The user wants to display these two datapoints:
-   * ... LP 1.Individual Voltages.Cell 1
-   * ... LP 1.Individual Temperatures.Cell 1
-   * The output is: "Individual Voltages Cell 1" and "Individual Temperatures.Cell 1"
-   * @param {object} datapoint
-   * @returns a unique label for a datapoint
-   */
-  const uniqueLabel = path => {
-    let numOccurrences = 0;
-    props.paths.forEach(p => {
-      if (isEqual(p[p.length - 1], path[path.length - 1])) {
-        numOccurrences++;
-      }
-    });
-    if (numOccurrences > 1 && path.length > 1) {
-      return [path[path.length - 2], path[path.length - 1]]
-        .join(" ")
-        .toUpperCase();
+  const getLabel = path => {
+    const parentKey = path[path.length - 2];
+    const key = path[path.length - 1];
+    if (parentKey === IGNORE_KEY) {
+      return capitalize(key);
+    } else {
+      return [parentKey, key].join(DELIMITER);
     }
-    return path[path.length - 1].toUpperCase();
   };
 
   /**
-   * Create an object containing all datasets, along with labels and line colors
+   * Create an object containing all data
    * Format:
    *  { datasets: [
    *      {
@@ -166,12 +94,9 @@ export default function LineGraph(props) {
     return {
       datasets: Array.from(props.paths, (path, i) => {
         return {
-          label: uniqueLabel(path),
-          data: datasets.hasOwnProperty(path) ? datasets[path] : [],
-          borderColor: graph_colors[i % graph_colors.length], // cycle colors
-          fill: false,
-          borderWidth: 1.5,
-          lineTension: 0
+          label: getLabel(path),
+          data: pathData.hasOwnProperty(path) ? pathData[path] : [],
+          borderColor: COLORS[i % COLORS.length] // cycle colors
         };
       })
     };
@@ -195,7 +120,6 @@ export default function LineGraph(props) {
       <div>
         <Line
           data={getData}
-          options={options(props.fontSize)}
           height={window.innerHeight / 4}
           width={window.innerWidth / 4.3}
         />
