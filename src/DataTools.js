487cf0ac723d46b2f81d3e4f69c3473232ee26d6
Filip Balucha <balucha.filip2@gmail.com>
2020-04-03 13:36:31 +0200

    Move functions working with pod data to DataTools

--- src/DataTools.js
+++ src/DataTools.js
@@ -1,3 +1,5 @@
+const DELIMITER = " > ";
+
 /**
  * Recursively walk the data object using a list of keys.
  *
@@ -15,18 +17,18 @@ const getDataPoint = (data, path) => {
     return undefined;
   }
   if (Array.isArray(data)) {
-    let key = path[0];
+    const key = path[0];
     path = path.slice(1);
     data = data.find(o => o.name === key);
   } else if (data.hasOwnProperty("value") && Array.isArray(data.value)) {
-    let key = path[0];
+    const key = path[0];
     path = path.slice(1);
     data = data.value.find(o => o.name === key);
   } else if (typeof data === "object" && data !== null) {
     if (path.length === 0) {
       return data;
     }
-    let key = path[0];
+    const key = path[0];
     data = data[key];
     path = path.slice(1);
   } else {
@@ -37,4 +39,37 @@ const getDataPoint = (data, path) => {
 
 export const getDataPointValue = (data, path) => {
   return getDataPoint(data, path).value;
+};
+
+/**
+ * Recursively walk the data object using a list of keys.
+ *
+ * @param {object} data – object to be walked
+ * @param {array} path – contains object keys
+ * @returns array of data point objects containing path and caption
+ */
+export const getAllPaths = (data, path = []) => {
+  if (
+    data.hasOwnProperty("crucial_data") &&
+    data.hasOwnProperty("additional_data")
+  ) {
+    return getAllPaths(data.crucial_data, ["crucial_data"]).concat(
+      getAllPaths(data.additional_data, ["additional_data"])
+    );
+  }
+  return data
+    .map(nestedData => {
+      if (Array.isArray(nestedData.value)) {
+        return getAllPaths(nestedData.value, [...path, nestedData.name]);
+      }
+      if (!isNaN(nestedData.value)) {
+        const p = [...path, nestedData.name];
+        return {
+          caption: p.join(DELIMITER),
+          path: p
+        };
+      }
+      return undefined;
+    })
+    .flat();
 };
--- src/components/DatapointContainer.js
+++ src/components/DatapointContainer.js
@@ -2,66 +2,31 @@ import React from "react";
 import "./DatapointContainer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-
-const DELIMITER = " > ";
+import { getAllPaths } from "../DataTools";
 
 export default function DatapointContainer(props) {
   // TODO: GLOBAL make everything Camel case
-
-  // TODO: can merge with getDataPoint???
-  /**
-   * Recursively walk the data object using a list of keys.
-   *
-   * @param {object} data – object to be walked
-   * @param {array} path – contains object keys
-   * @returns array of data point objects containing path and caption
-   */
-  const getDataPoints = (data, path = []) => {
-    if (
-      data.hasOwnProperty("crucial_data") &&
-      data.hasOwnProperty("additional_data")
-    ) {
-      return getDataPoints(data.crucial_data, ["crucial_data"]).concat(
-        getDataPoints(data.additional_data, ["additional_data"])
-      );
-    }
-    return data
-      .map(nestedData => {
-        if (Array.isArray(nestedData.value)) {
-          return getDataPoints(nestedData.value, [...path, nestedData.name]);
-        }
-        if (!isNaN(nestedData.value)) {
-          let p = [...path, nestedData.name];
-          return {
-            caption: p.join(DELIMITER),
-            path: p
-          };
-        }
-        return undefined;
-      })
-      .flat();
-  };
 
   /**
    * Create an array of data point divs
    *
    * @returns an array of data point divs
    */
-  const getDataPointList = () => {
-    const dataPoints = getDataPoints(props.data);
-    return dataPoints
-      .filter(dataPoint => dataPoint !== undefined)
-      .map((dataPoint, i) => {
+  const getPathList = () => {
+    const paths = getAllPaths(props.data);
+    return paths
+      .filter(path => path !== undefined)
+      .map((path, i) => {
         return (
           <div
             className={[
-              props.isSelected(dataPoint.path) ? "datapoint--selected" : "",
+              props.isSelected(path.path) ? "datapoint--selected" : "",
               "datapoint"
             ].join(" ")}
-            onClick={() => props.onDataPointClicked(dataPoint.path)}
+            onClick={() => props.onDataPointClicked(path.path)}
             key={i}
           >
-            {dataPoint.caption}
+            {path.caption}
           </div>
         );
       });
@@ -76,7 +41,7 @@ export default function DatapointContainer(props) {
         onClick={props.onCloseClicked}
         icon={faTimes}
       />
-      <div className="content">{getDataPointList()}</div>
+      <div className="content">{getPathList()}</div>
     </div>
   );
 }
